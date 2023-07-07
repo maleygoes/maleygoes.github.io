f53a872c054d389b3f0872b241c2a9f14a6bc652
goruda <maleysp@gmail.com>
2023-07-07 14:08:43 -0400

    asd

---
+++ CalcContent/lunr-pretext-search-index.js
@@ -0,0 +1,211 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "   example.org   https:\/\/example.org   copyright  "
+},
+{
+  "id": "section1.1",
+  "level": "1",
+  "url": "section1.1.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "The Origins of Calculus",
+  "body": " The Origins of Calculus  Content Coming...  "
+},
+{
+  "id": "section1.2",
+  "level": "1",
+  "url": "section1.2.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "The Fundamental Concepts",
+  "body": " The Fundamental Concepts  Content Coming...  "
+},
+{
+  "id": "section1.3",
+  "level": "1",
+  "url": "section1.3.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Applications of Calculus in Real Life",
+  "body": " Applications of Calculus in Real Life  Content Coming...  "
+},
+{
+  "id": "section2.1",
+  "level": "1",
+  "url": "section2.1.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Limits and Continuity",
+  "body": " Limits and Continuity  Content Coming...  "
+},
+{
+  "id": "section2.2",
+  "level": "1",
+  "url": "section2.2.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Differentiation Rules",
+  "body": " Differentiation Rules  Content Coming...  "
+},
+{
+  "id": "section2.3",
+  "level": "1",
+  "url": "section2.3.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Derivatives of Basic Functions",
+  "body": " Derivatives of Basic Functions  Content Coming...  "
+},
+{
+  "id": "exponential-functions-lesson",
+  "level": "1",
+  "url": "exponential-functions-lesson.html",
+  "type": "Section",
+  "number": "2.4",
+  "title": "Exponential Functions Derivatives",
+  "body": " Exponential Functions Derivatives   Why do I need to know this?  Exponential functions are everywhere, as they represent anything that grows in proportion to the present amount. They are also pivotal in Differential equations which are really what describe reality as we know it physically.    What will we learn?  By the end of this lesson, you will be able to confidently calculate the derivative of exponential functions of the form and , where is a constant positive number. You will gain a deep understanding of how the derivative describes the behavior of these functions.    What will I need to recall in order to learn?  Before we dive into exponential function derivatives, recall how the derivative represents the rate of change and the slope of a function at a given point. You may wish to review the limit definition of the derivative .    The Main Idea  In this section, we'll explore the properties and behavior of exponential functions. We'll discuss how exponential functions grow or decay exponentially and examine their unique characteristics. We'll also introduce the concept of the derivative and its connection to the rate of change.   Lecture Video  Start at 13:45 and watch until 20:30.      Worked Example  Find the derivative of  To find the derivative of , we will use the chain rule.  Let's start by noticing the inner function, we can call it . Now, our function looks like .  Next, we need to find the derivative of with respect to . Using the chain rule, we have:     The derivative of with respect to is simply . This is why calc students do, and should, love as a function. Now, let's find .  Differentiating with respect to gives us:     Finally, we can substitute the derivative of and back into the chain rule expression:     Since , we can rewrite the expression as:     **Sometimes you will see me make a substitution for and use the chain rule. Sometimes we will skip this step. It is important to just remember the concept -- if there is an inner function, we need to remember to \"respect\" the chain rule.    Continue the conversation on Discourse  Questions and worked examples found in Discourse-- https:\/\/maleymath.discourse.group\/t\/exponential-function-derivatives-6-2\/208?u=profmaley   "
+},
+{
+  "id": "section2.4",
+  "level": "1",
+  "url": "section2.4.html",
+  "type": "Section",
+  "number": "2.5",
+  "title": "Applications of Differentiation",
+  "body": " Applications of Differentiation  Content Coming...  "
+},
+{
+  "id": "section3.1",
+  "level": "1",
+  "url": "section3.1.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Chain Rule",
+  "body": " Chain Rule  Content Coming...  "
+},
+{
+  "id": "section3.2",
+  "level": "1",
+  "url": "section3.2.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Implicit Differentiation",
+  "body": " Implicit Differentiation  Content Coming...  "
+},
+{
+  "id": "section3.3",
+  "level": "1",
+  "url": "section3.3.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Related Rates",
+  "body": " Related Rates  Lesson: Related Rates  Why Do I Need to Know This?  Related rates are an invaluable application of calculus. Usually, a complex problem has not only one quantity changing, but multiple components within a system that are related somehow. This is the intention behind this unit. How do we measure change within a system of related, changing quantities?  What Will We Learn?   Identify and solve related rates problems  Apply the concept of related rates to analyze changing quantities  Develop problem-solving skills for real-world scenarios   What Should I Recall Before Learning?  Before we dive into related rates, let's recall some fundamental concepts in calculus, such as rates of change, derivatives, and the chain rule.Understanding these concepts will provide a solid foundation for tackling related rates problems.  The Main Idea  Introduction to Related Rates  Related rates involve finding how the rates of change of two or more related variables are related to each other. We often encounter these types of problems in real-world scenarios where the rates at which quantities change depend on each other.  Problem-solving Approach  To solve related rates problems, we generally follow these steps:   Identify the variables involved and establish their relationships (e.g. generate sketches, and equations)  Differentiate the related equation(s) implicitly with respect to time or the independent variable.  Substitute known values, and use algebra to solve for the unknown rate of change.   Lecture Video   Practice   Interactive Conical Tank  Here is an interactive that can help you visualize what's happening within a common problem in related rates: The conical tank.  Note you can adjust the fill rate (to filling or draining) and see the effects on dr\/dt and dV\/dt.  https:\/\/www.geogebra.org\/m\/wZ3YyCE2  Practice and Feedback  Head to Discourse with questions. There will be a lot of places where algebra, geometry, or problem-solving will be rusty for each of us. Make sure to start this unit early to benefit maximally from Q&A.  "
+},
+{
+  "id": "section3.4",
+  "level": "1",
+  "url": "section3.4.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Higher Order Derivatives",
+  "body": " Higher Order Derivatives  Content Coming...  "
+},
+{
+  "id": "section4.1",
+  "level": "1",
+  "url": "section4.1.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Optimization Problems",
+  "body": " Optimization Problems  Content Coming...  "
+},
+{
+  "id": "section4.2",
+  "level": "1",
+  "url": "section4.2.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Curve Sketching",
+  "body": " Curve Sketching  Content Coming...  "
+},
+{
+  "id": "linear-approximation-differentials-lesson",
+  "level": "1",
+  "url": "linear-approximation-differentials-lesson.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Lesson: Linear Approximation and Differentials",
+  "body": " Lesson: Linear Approximation and Differentials   Why do I need to know this?  Imagine you are faced with a complex function, and you need to understand its behavior or estimate its value quickly. How can calculus help you in such situations? In this lesson, we will explore the powerful techniques of linear approximation and differentials that allow us to simplify functions and make accurate estimates.    What will I learn?  By the end of this lesson, you will be able to:   Apply linear approximation to approximate the value of a function near a given point  Understand the concept of differentials and their significance in calculus  Use differentials to estimate the change in a function based on small changes in its variables     What should I recall in order to learn this?  Before we dive into linear approximation and differentials, let's recall some fundamental concepts in calculus, such as derivatives, tangent lines, and rates of change. Understanding these concepts will provide a solid foundation for tackling the techniques discussed in this lesson.   Equation of the tangent line to at :     The Main Idea(s)   Introduction to Linear Approximation  Linear approximation allows us to approximate the value of a function near a specific point using a simpler linear function. By finding the equation of the tangent line at that point, we can make accurate estimations within a small range.    Calculating Linear Approximations  Learn how to find the linear approximation of a function using the tangent line equation. Understand the relationship between the linear approximation and the original function and how it can be used for estimation and simplification.    Introduction to Differentials  Differentials are infinitesimally small changes in a function's variables. They play a crucial role in calculus, allowing us to approximate the change in a function based on these small changes in its inputs.    Using Differentials for Estimation  Explore how differentials can be used to estimate the change in a function. Learn how to calculate differentials, interpret their meaning, and apply them to solve estimation problems.     Lecture Video     Practice Exercises  Practice set of problems in Webassign. Use Discourse on the problems that stump you!    Applications and Connections  One interesting and real application of tangent line approximations in engineering is in the field of structural analysis. Engineers often use linear approximation techniques to estimate the behavior of structures under different loading conditions.  For example, when designing bridges or buildings, engineers need to assess the structural integrity and stability of the components. By using tangent line approximations, they can estimate the deflection, stress, and strain on different parts of the structure. From there, they can make informed decisions about materials, dimensions, support systems, and so on.   "
+},
+{
+  "id": "section4.4",
+  "level": "1",
+  "url": "section4.4.html",
+  "type": "Section",
+  "number": "4.4",
+  "title": "Newton's Method",
+  "body": " Newton's Method  Content Coming...  "
+},
+{
+  "id": "section5.1",
+  "level": "1",
+  "url": "section5.1.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Limits and Continuity",
+  "body": " Limits and Continuity  Content Coming...  "
+},
+{
+  "id": "section5.2",
+  "level": "1",
+  "url": "section5.2.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Integration Rules",
+  "body": " Integration Rules  Content Coming...  "
+},
+{
+  "id": "section5.3",
+  "level": "1",
+  "url": "section5.3.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Methods of Integration",
+  "body": " Methods of Integration  Content Coming...  "
+},
+{
+  "id": "section5.4",
+  "level": "1",
+  "url": "section5.4.html",
+  "type": "Section",
+  "number": "5.4",
+  "title": "Applications of Integration",
+  "body": " Applications of Integration  Content Coming...  "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
